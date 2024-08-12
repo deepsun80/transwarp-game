8eaba76018d37ff8b735b8d1dd8c54e8463968b6
Sandeep Chandran <deepsun80@gmail.com>
2024-08-12 09:57:36 -0500

    update raycast

--- src/helpers/useForwardRaycast.tsx
+++ src/helpers/useForwardRaycast.tsx
@@ -1,9 +1,13 @@
 import { useThree } from '@react-three/fiber';
 import { useMemo } from 'react';
 import { Object3D, Raycaster, Vector3 } from 'three';
+import * as THREE from 'three';
 
 export const useForwardRaycast = (obj: { current: Object3D | null }) => {
-  const raycaster = useMemo(() => new Raycaster(), []);
+  const raycaster = useMemo(
+    () => new Raycaster(new THREE.Vector3(), new THREE.Vector3(), 0, 0.1),
+    []
+  );
   const pos = useMemo(() => new Vector3(), []);
   const dir = useMemo(() => new Vector3(), []);
   const scene = useThree((state) => state.scene);
